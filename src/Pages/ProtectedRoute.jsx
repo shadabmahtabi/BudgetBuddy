b363cc713cb2a1e26143b6ddaab0566d194ea47a
Shadab Mahtabi <shadabmanpuri@gmail.com>
2024-08-24 21:22:36 +0530

    loader integeration

--- src/Pages/ProtectedRoute.jsx
+++ src/Pages/ProtectedRoute.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { Navigate, Outlet } from "react-router-dom";
+import Loader from "../components/Loader";
 
 const ProtectedRoute = ({ redirectPath = "/login" }) => {
-  const { token } = useSelector((state) => state.user);
+  const { token, user, loading } = useSelector((state) => state.user);
   // console.log(token);
 
   if (!token) {
     return <Navigate to={redirectPath} />;
+  }
+
+  if(loading && user === null) {
+    return <Loader />;
   }
 
   return <Outlet />;
--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,7 +1,7 @@
 import css from "./Loader.module.css";
 import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 
-const Loader = ({login}) => {
+const Loader = () => {
   return (
     <div className={css.main}>
       <DotLottieReact
@@ -13,7 +13,7 @@ https://lottie.host/5a62f233-60c3-41ff-bd22-9299f0e1b898/x0RHMJbOlb.json
         className={css.loaderImg}
       />
       <h3>
-        {login === "" ? "Connecting to server" : login}<span className={css.dot}>.</span>
+        Connecting to server<span className={css.dot}>.</span>
         <span className={css.dot}>.</span>
         <span className={css.dot}>.</span>
       </h3>
